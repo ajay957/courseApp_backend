28ff7b6b206e67671c7255da26f2fe456aa72f2d
Ajay957 <ajay74.aa82@gmail.com>
2022-12-06 14:45:37 +0530

    first commit

---
+++ src/main/java/com/example/courseapp/dao/CourseDao.java
@@ -0,0 +1,7 @@
+package com.example.courseapp.dao;
+
+import com.example.courseapp.model.Courses;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseDao extends CrudRepository<Courses,Integer> {
+}
