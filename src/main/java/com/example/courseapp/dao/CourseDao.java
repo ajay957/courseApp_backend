f2b030f07517bd9ba89b5a8f532a211a162e2967
Ajay957 <ajay74.aa82@gmail.com>
2022-12-09 03:05:56 +0530

    flexible search added

--- src/main/java/com/example/courseapp/dao/CourseDao.java
+++ src/main/java/com/example/courseapp/dao/CourseDao.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CourseDao extends CrudRepository<Courses,Integer> {
-    @Query(value = "SELECT `id`, `course_title`, `date`, `description`, `duration`, `venue` FROM `courses` WHERE `course_title`= :course_title",nativeQuery = true)
+    @Query(value = "SELECT `id`, `course_title`, `date`, `description`, `duration`, `venue` FROM `courses` WHERE `course_title` like %:course_title%",nativeQuery = true)
     List<Courses> SearchCourse(@Param("course_title") String course_title);
 }
